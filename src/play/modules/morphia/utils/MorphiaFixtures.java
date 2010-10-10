2dd6c4b6210d7cc695bb8232ddebb2a5f30c3362
greenlaw110 <greenlaw110@gmail.com>
2010-10-10 20:42:08 +0800

    update .gitignore

---
+++ src/play/modules/morphia/utils/MorphiaFixtures.java
@@ -0,0 +1,38 @@
+package play.modules.morphia.utils;
+
+import java.util.List;
+
+import play.Play;
+import play.modules.morphia.Model;
+import play.modules.morphia.MorphiaPlugin;
+import play.test.Fixtures;
+
+import com.google.code.morphia.Datastore;
+
+public class MorphiaFixtures extends Fixtures {
+    private static Datastore ds() {
+        return MorphiaPlugin.ds();
+    }
+    public static void deleteAll() {
+        Datastore ds = ds();
+        for (Class<Model> clz: Play.classloader.getAssignableClasses(Model.class)) {
+            ds.getCollection(clz).drop();
+        }
+    }
+    
+    public static void delete(Class<? extends Model> ... types) {
+        for (Class<? extends Model> type: types) {
+            ds().getCollection(type).drop();
+        }
+    }
+    
+    public static void delete(List<Class<Model>> classes) {
+        for (Class<Model> type: classes) {
+            ds().getCollection(type).drop();
+        }
+    }
+    
+    public static void deleteAllModels() {
+        deleteAll();
+    }
+}
