6c8311b2e8a9049fb9b8a6a064e5dbc839ef254f
greenlaw110 <greenlaw110@gmail.com>
2012-02-13 22:55:16 +1100

    bug fix: #56

--- samples-and-tests/unit-tests/app/models/Base.java
+++ samples-and-tests/unit-tests/app/models/Base.java
@@ -4,21 +4,10 @@ import org.bson.types.ObjectId;
 
 import play.modules.morphia.Model;
 
+import com.google.code.morphia.annotations.Entity;
 import com.google.code.morphia.annotations.Id;
 
 
 @SuppressWarnings("serial")
 public abstract class Base extends Model {
-	@Id
-	public ObjectId _id;
-	
-	protected Base() {_id = new ObjectId();}
-	
-	protected Base(ObjectId id) {_id = id;}
-	
-	@Override public Object getId() {return _id;}
-	
-	@Override protected void setId_(Object id) {_id = (ObjectId)processId_(id);}
-	
-	protected static Object processId_(Object id) {return (id instanceof ObjectId) ? id : new ObjectId(id.toString());}
 }
--- src/play/modules/morphia/Model.java
+++ src/play/modules/morphia/Model.java
@@ -618,7 +618,7 @@ public class Model implements Serializable, play.db.Model {
      * Shortcut to find(String, Object...)
      * 
      * @param keys
-     * @param objects
+     * @param keys
      * @return
      */
     public static <T extends Model> MorphiaQuery q(String keys, Object value) {
@@ -814,8 +814,8 @@ public class Model implements Serializable, play.db.Model {
     protected void loadBlobs() {
         // used by enhander
     }
-    
-    protected final Map<String, Boolean> blobFieldsTracker = new HashMap<String, Boolean>();
+
+    transient protected final Map<String, Boolean> blobFieldsTracker = new HashMap<String, Boolean>();
     protected final boolean blobChanged(String fieldName) {
         return (blobFieldsTracker.containsKey(fieldName) && blobFieldsTracker.get(fieldName));
     }
@@ -1346,7 +1346,7 @@ public class Model implements Serializable, play.db.Model {
      * NoID is used to annotate on sub types which is sure to get ID field from
      * parent type
      * 
-     * @see https://groups.google.com/d/topic/play-framework/hPWJCvefPoI/discussion
+     * @see //groups.google.com/d/topic/play-framework/hPWJCvefPoI/discussion
      *      
      * @author luog
      */
