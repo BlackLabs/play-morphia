7174f883e02d49c0606fe9aae926376811c883f6
yepelinux <marianoyepes@gmail.com>
2023-01-07 19:29:42 -0300

    cambios

--- src/play/modules/morphia/AggregationResult.java
+++ src/play/modules/morphia/AggregationResult.java
@@ -1,70 +1,75 @@
 package play.modules.morphia;
 
-import org.osgl.util.S;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+
 import com.mongodb.BasicDBObject;
 
-import java.util.*;
-
 public class AggregationResult {
-    private List<BasicDBObject> r_ = null;
-    private Class<? extends Model> c_ = null;
-    private String def_ = null;
-    private String mappedDef_ = null;
+  private List<BasicDBObject> r_ = null;
+  private Class<? extends Model> c_ = null;
+  private String def_ = null;
+  private String mappedDef_ = null;
 
-    public AggregationResult(List<BasicDBObject> r, String aggregationField, Class<? extends Model> modelClass) {
-        if (null == r || null == aggregationField) throw new NullPointerException();
-        r_ = r;
-        def_ = aggregationField;
-        c_ = modelClass;
-        mappedDef_ = MorphiaPlugin.mongoColName(modelClass, aggregationField);
+  public AggregationResult(List<BasicDBObject> r, String aggregationField, Class<? extends Model> modelClass) {
+    if (null == r || null == aggregationField) throw new NullPointerException();
+    r_ = r;
+    def_ = aggregationField;
+    c_ = modelClass;
+    mappedDef_ = MorphiaPlugin.mongoColName(modelClass, aggregationField);
+  }
+
+  private static boolean isEqual_(Object a, Object b) {
+    if (a == b) return true;
+    return (null == a) ? (null == b) : (null == b) ? false : a.equals(b);
+  }
+
+  public Long getResult() {
+    return r_.size() > 0 ? r_.get(0).getLong(mappedDef_) : null;
+  }
+
+  public Long getResult(String groupKeys, Object... groupValues) {
+    if (StringUtils.isBlank(groupKeys)) {
+      if (groupValues.length == 0) return getResult();
+      throw new IllegalArgumentException("the number of group keys does not match the number of group values");
     }
-
-    private static boolean isEqual_(Object a, Object b) {
-        if (a == b) return true;
-        return (null == a) ? (null == b) : (null == b) ? false : a.equals(b);
-    }
-
-    public Long getResult() {
-        return r_.size() > 0 ? r_.get(0).getLong(mappedDef_) : null;
-    }
-
-    public Long getResult(String groupKeys, Object... groupValues) {
-        if (S.empty(groupKeys)) {
-            if (groupValues.length == 0) return getResult();
-            throw new IllegalArgumentException("the number of group keys does not match the number of group values");
+    String[] sa = groupKeys.split("(And|[,;\\s])");
+    if (sa.length != groupValues.length) throw new IllegalArgumentException("the number of group keys does not match the number of group values");
+    for (BasicDBObject r: r_) {
+      boolean found = true;
+      String s;
+      for (int i = 0; i < sa.length; ++i) {
+        s = MorphiaPlugin.mongoColName(c_, sa[i]);
+        if (!isEqual_(r.get(s), groupValues[i])) {
+          found = false;
+          break;
         }
-        String[] sa = groupKeys.split("(And|[,;\\s])");
-        if (sa.length != groupValues.length) throw new IllegalArgumentException("the number of group keys does not match the number of group values");
-        for (BasicDBObject r: r_) {
-            boolean found = true;
-            String s;
-            for (int i = 0; i < sa.length; ++i) {
-                s = MorphiaPlugin.mongoColName(c_, sa[i]);
-                if (!isEqual_(r.get(s), groupValues[i])) {
-                    found = false;
-                    break;
-                }
-            }
-            if (found) return r.getLong(mappedDef_);
-        }
-        return null;
+      }
+      if (found) return r.getLong(mappedDef_);
     }
-    
-    public Map<String, Long> asNumberMap() {
-        Map<String, Long> m = new HashMap(r_.size());
-        for (BasicDBObject r : r_) {
-            Collection<?> c = r.values();
-            Iterator itr = c.iterator();
-            String k = itr.next().toString();
-            String s = itr.next().toString();
-            float f = Float.parseFloat(s);
-            long l = (long)f;
-            m.put(k, l);
-        }
-        return m;
+    return null;
+  }
+
+  public Map<String, Long> asNumberMap() {
+    Map<String, Long> m = new HashMap(r_.size());
+    for (BasicDBObject r : r_) {
+      Collection<?> c = r.values();
+      Iterator itr = c.iterator();
+      String k = itr.next().toString();
+      String s = itr.next().toString();
+      float f = Float.parseFloat(s);
+      long l = (long)f;
+      m.put(k, l);
     }
-    
-    public List<BasicDBObject> raw() {
-        return r_;
-    }
+    return m;
+  }
+
+  public List<BasicDBObject> raw() {
+    return r_;
+  }
 }
